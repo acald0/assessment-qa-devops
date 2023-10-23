eca119a5e8a72d12b2a4e625d660adaf9f870b2b
acald0 <annecaldwell2002@gmail.com>
2023-10-22 21:17:51 -0600

    finished part 4

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,19 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test("draw button gives bot choices", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id('draw')).click()
+    const isChoices = await driver.findElement(By.id('choices')).isDisplayed()
+    expect(isChoices).toBe(true)
+  });
+
+  test("add to duo displays correct div", async () => {
+    await driver.get("http://localhost:8000")
+    await driver.findElement(By.id('draw')).click()
+    await driver.findElement(By.className('bot-btn')).click()
+    const divIs = await driver.findElement(By.id('player-duo')).isDisplayed()
+    expect(divIs).toBe(true)
+  })
 });
